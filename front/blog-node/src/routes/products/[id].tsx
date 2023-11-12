71c7f0895992c03a1176ca11ab9c0803680f4a17
tamir.rust <timorust1@gmail.com>
2023-11-13 01:30:42 +0200

    checkIsNumberZod

--- front/blog-node/src/routes/products/[id].tsx
+++ front/blog-node/src/routes/products/[id].tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { z } from "zod";
 
 export function ProductId() {
   const params = useParams();
+  const checkIsNumberZod = z.string().refine((value) => {
+    return !isNaN(Number(value));
+  });
 
-  return <h1>Product ID {JSON.stringify(params)}</h1>;
+  const res = checkIsNumberZod.safeParse(params.id);
+  if (!res.success) return <h1>error params {String(res.error)}</h1>;
+  const id = parseInt(res.data);
+
+  return <h1>Product ID {id}</h1>;
 }
